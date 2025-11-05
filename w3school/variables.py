80c4f28ee6d95ef3a2d001d2f3fb7abe2b586264
Bonvas <bonvas73@gmail.com>
2025-11-05 06:11:43 +0200

    Add file variables

---
+++ w3school/variables.py
@@ -0,0 +1,19 @@
+# declare three variables (name, price, stock status) for a product using snake_case.
+# print declared variables and their type.
+# in a single line of code, assign the values 20, 58, and 97 to the variables x, y, and z.
+# pring variables x, y, z.
+
+product_name = "Laptop"
+product_price = 1200.99
+is_in_stock = True
+
+print(product_name)
+print(product_price)
+print(is_in_stock)
+print(type(product_name))
+print(type(product_price))
+print(type(is_in_stock))
+
+x, y, z = 20, 58, 97
+print(x, y, z)
+
