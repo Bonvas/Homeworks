048acaa6ecf974017261e9952374e3f306b2d5f1
Bonvas <bonvas73@gmail.com>
2025-11-05 06:13:12 +0200

    Add file lists

---
+++ w3school/lists.py
@@ -0,0 +1,82 @@
+# Task 1 start
+# creat different lists with data and print it
+# second option for first list: city_1 = city_2 = city_3 = "Lviv":
+name_1 = "Vasyl"
+age_1 = 52
+city_1 = "Lviv"
+
+name_2 = "Daniel"
+age_2 = 23
+city_2 = "Lviv"
+
+name_3 = "George"
+age_3 = 20
+city_3 = "Lviv"
+
+list_men = [name_1, age_1, city_1, name_2, age_2, city_2, name_3, age_3, city_3]
+print(list_men)
+print(type(list_men))
+# Task 1 end
+
+# Task 2 start
+# create list and print him
+a = "apple"
+b = "banana"
+o = "orange"
+p = "pear"
+l = "lemon"
+g = "grapes"
+w = "watermelon"
+k = "kiwi"
+
+fruits = [a, b, o, p, l, g, w, k]
+print(fruits)
+print(type(fruits))
+# Task 2 end
+
+# Task 3 start
+# create list and print him
+num_1 = 10
+number = 34.75
+bool = True
+num_2 = 0.4587
+x = "Spring"
+
+any_list = [num_1, number, bool, num_2, x]
+print(any_list)
+print(type(any_list))
+# Task 3 end
+
+# Task 4 start
+# create list and print him
+col_1 = "Red"
+col_2 = "White"
+col_3 = "Yellow"
+col_4 = "Green"
+col_5 = "Black"
+col_6 = "Blue"
+
+list_color = [col_1, col_2, col_3, col_4, col_5, col_6]
+print(list_color)
+print(type(list_color))
+# Task 4 end
+
+# Task 5 start
+# create list and print him
+word_1 = "Python"
+word_2 = "lists"
+word_3 = "are"
+word_4 = "powerful."
+
+sentence = [word_1, word_2, word_3, word_4]
+print(sentence)
+print(type(sentence))
+print(word_1, word_2, word_3, word_4)
+# Task 5 end
+
+# Task 6 start
+# create blank list
+n = []
+print(n)
+print(type(n))
+# Task 6 end
