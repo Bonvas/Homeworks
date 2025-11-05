d74f6d30903965cc0fa1084e5807e95f4b2ae0f8
Bonvas <bonvas73@gmail.com>
2025-11-05 05:28:46 +0200

    Add file exercise7

---
+++ PYnative for beginners/exercise7.py
@@ -0,0 +1,8 @@
+# Find the number of occurrences of a substing in a string
+
+str_x = "Emma is good developer. Emma is a writer."
+cnt = str_x.count("Emma") # use count method of a str class
+print(cnt) # print result
+
+cnt = str_x.count("e") # use count method of a str class
+print(cnt) # print result
