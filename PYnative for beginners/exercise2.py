d0c3e106ba8b0fea4e9335e9a940c95ffdd346a7
Bonvas <bonvas73@gmail.com>
2025-11-05 05:24:02 +0200

    Add file exercise2

---
+++ PYnative for beginners/exercise2.py
@@ -0,0 +1,10 @@
+# Write Python code to iterate through the first 10 numbers and, in each iteration,
+# print the sum of the current and previous number.
+
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
+
+for i in range(1, 11):
+    x_sum = previous_num + i
+    print("Current number", i, "Previous number ", previous_num, " Sum: ", x_sum)
+    previous_num = i
