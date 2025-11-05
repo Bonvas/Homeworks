41e4f3d856a778b79f174901bfd3ddebb8ed4c11
Bonvas <bonvas73@gmail.com>
2025-11-05 07:02:46 +0200

    Add file exercise10

---
+++ PYnative for beginners/exercise10.py
@@ -0,0 +1,26 @@
+# Merge two lists using the following condition.
+# Given two lists of numbers, write Python code to create a new 
+# list containing odd numbers from the first list and even
+# numbers from the second list.
+
+def merge_list(list1, list2):
+    result_list = []
+
+    # iterate first list
+    for num in list1:
+        # check if current number is odd
+        if num % 2 != 0:
+            # add odd number to result list
+            result_list.append(num)
+
+    # iterate second list
+    for num in list2:
+        # check if current number is even
+        if num % 2 == 0:
+            # add even number to result list
+            result_list.append(num)
+    return result_list
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+print("Result list:", merge_list(list1, list2))
